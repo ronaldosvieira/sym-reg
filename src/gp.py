426cb68a8b33fa89a0275897cc777f393fc340bc
Ronaldo Vieira <ronaldoesv@gmail.com>
2018-09-16 14:13:31 +0000

    Adds pretty str() for trees

--- src/gp.py
+++ src/gp.py
@@ -5,23 +5,31 @@ class Node:
     pass
 
 class UnaryOperator(Node):
-    def __init__(self, f, child = None):
+    def __init__(self, f, child = None, str_rep = 'unop({})'):
         self.f = f
         self.arity = 1
         self.child = child
+        self.str_rep = str_rep
         
     def evaluate(self, **values):
         return self.f(self.child.evaluate(**values))
+        
+    def __str__(self):
+        return self.str_rep.format(str(self.child))
 
 class BinaryOperator(Node):
-    def __init__(self, f, left = None, right = None):
+    def __init__(self, f, left = None, right = None, str_rep = 'binop({}, {})'):
         self.f = f
         self.arity = 2
         self.left = left
         self.right = right
+        self.str_rep = str_rep
     
     def evaluate(self, **values):
         return self.f(self.left.evaluate(**values), self.right.evaluate(**values))
+
+    def __str__(self):
+        return self.str_rep.format(str(self.left), str(self.right))
 
 class ConstantTerminal(Node):
     def __init__(self, constant):
@@ -29,6 +37,9 @@ class ConstantTerminal(Node):
     
     def evaluate(self, **values):
         return self.constant
+        
+    def __str__(self):
+        return str(self.constant)
 
 class VariableTerminal(Node):
     def __init__(self, variable):
@@ -39,6 +50,9 @@ class VariableTerminal(Node):
             return values[self.variable]
         except:
             raise ValueError("%s value not found" % self.variable)
+            
+    def __str__(self):
+        return str(self.variable)
 
 def random_constant(start, end):
     return ConstantTerminal(np.random.randint(start, end))
