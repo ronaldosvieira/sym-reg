5d71bdac28eb48d95b9c9311a911c4f37fb7a2dc
Ronaldo Vieira <ronaldoesv@gmail.com>
2018-09-16 13:22:33 +0000

    Creates generators for fitness function

--- src/main.py
+++ src/main.py
@@ -8,25 +8,35 @@ from gp import *
 def read_dataset(path):
     return pd.read_csv(path, sep = ',', names = ['x', 'y', 'f'])
 
-def eval_fitness(individual, dataset):
-    pred = dataset.apply(lambda row: individual.evaluate(**row), axis = 1)
-    truth = dataset['f']
-    
-    residuals = np.square(truth - pred)
-    normalizer = np.square(dataset['f'] - dataset['f'].mean())
-    
-    return np.sqrt(np.divide(np.sum(residuals), np.sum(normalizer)))
+def get_nrmse(dataset):
+    def nrmse(individual):
+        pred = dataset.apply(lambda row: individual.evaluate(**row), axis = 1)
+        truth = dataset['f']
+        
+        residuals = np.square(truth - pred)
+        normalizer = np.square(dataset['f'] - dataset['f'].mean())
+        
+        return np.sqrt(np.divide(np.sum(residuals), np.sum(normalizer)))
+        
+    return nrmse
 
-def eval_fitness_batch(individuals, dataset):
-    pred = pd.DataFrame(list(map(
-        lambda ind: dataset.apply(lambda row: ind.evaluate(**row), axis = 1), 
-        individuals))).T
-    truth = dataset['f']
+def get_batch_nrmse(dataset):
+    def batch_nrmse(individuals):
+        pred = pd.DataFrame(list(map(
+            lambda ind: dataset.apply(
+                lambda row: ind.evaluate(**row), 
+                axis = 1), 
+            individuals))).T
+        truth = dataset['f']
+        
+        residuals = np.square((truth - pred.T).T)
+        normalizer = np.square(dataset['f'] - dataset['f'].mean())
+        
+        return np.sqrt(np.divide(
+            np.sum(residuals, axis = 0), 
+            np.sum(normalizer)))
     
-    residuals = np.square((truth - pred.T).T)
-    normalizer = np.square(dataset['f'] - dataset['f'].mean())
-    
-    return np.sqrt(np.divide(np.sum(residuals, axis = 0), np.sum(normalizer)))
+    return batch_nrmse
 
 train = read_dataset('data/synth1/synth1-train.csv')
 
