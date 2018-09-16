9d749f50d8a0d994b822a388acb3822cfdf05ff5
Ronaldo Vieira <ronaldoesv@gmail.com>
2018-09-16 13:10:58 +0000

    Creates NRMSE fitness eval. functions

--- src/main.py
+++ src/main.py
@@ -8,6 +8,31 @@ from gp import *
 def read_dataset(path):
     return pd.read_csv(path, sep = ',', names = ['x', 'y', 'f'])
 
+def eval_fitness(individual, dataset):
+    pred = dataset.apply(lambda row: individual.evaluate(**row), axis = 1)
+    truth = dataset['f']
+    
+    residuals = np.square(truth - pred)
+    normalizer = np.square(dataset['f'] - dataset['f'].mean())
+    
+    return np.sqrt(np.divide(np.sum(residuals), np.sum(normalizer)))
+
+def eval_fitness_batch(individuals, dataset):
+    pred = pd.DataFrame(list(map(
+        lambda ind: dataset.apply(lambda row: ind.evaluate(**row), axis = 1), 
+        individuals))).T
+    truth = dataset['f']
+    
+    residuals = np.square((truth - pred.T).T)
+    normalizer = np.square(dataset['f'] - dataset['f'].mean())
+    
+    return np.sqrt(np.divide(np.sum(residuals, axis = 0), np.sum(normalizer)))
+
 train = read_dataset('data/synth1/synth1-train.csv')
 
-print(train)
+tree = BinaryOperator(lambda x, y: x + y,
+        VariableTerminal('x'),
+        ConstantTerminal(5))
+        
+print(eval_fitness(tree, train))
+print(eval_fitness_batch([tree, tree, tree], train))
